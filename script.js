5e4ccdb2cffa045e1ee805e49f3eb7da7bbf261f
vaishnavi-panchal0108 <vaishnavipanchal0108@gmail.com>
2026-06-24 18:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let modal = document.getElementById("modal");
+
+document.getElementById("openBtn").onclick = function () {
+  modal.style.display = "block";
+};
+
+document.getElementById("closeBtn").onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
